37989988cc3999c0462c053817142ad31425dc9c
stevekutz <stkutz@gmail.com>
2019-11-04 22:45:09 -0600

    building form and handlers

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Button, Card, Form, Input, Label,}
+import {Button, Card, Form, Input, Label} from 'semantic-ui-react';
 import './App.css';
 
 const App = () => {
@@ -31,11 +31,17 @@ const App = () => {
   return (
     <div className="App">
       <Form>
-      
-      
+        <Label> New Todo Item</Label>
+        <Input
+          label = 'new thing to do'
+          placeholder = 'the placeholer text'
+          style = {}
+          value = {}
+          onChange = {handleChange}
+        
+        />
+        <Button type = 'submit'> add Todo </Button>
       </Form>
-
-
     </div>
   );
 }
