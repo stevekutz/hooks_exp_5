0f56586686abea893421a0ec612227a83c405593
stevekutz <stkutz@gmail.com>
2019-11-05 22:29:55 -0600

    added completion status and logic to prevent blank todos being added

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,23 @@
-import React, {useState, useEffect} from 'react';
+// import React, {useState, useEffect} from 'react';
+import React, {useEffect} from 'react';
+import {StateInspector, useState} from 'reinspect';
+
 import {Button, Card, Form, Input, Label} from 'semantic-ui-react';
 import './App.css';
 
-const App = () => {
-  const [task, setTask] = useState('');
-  const [todos, setTodos] = useState([]);
+const App = ({id}) => {
+  const [task, setTask] = useState('', id);
+  const [todos, setTodos] = useState([], id);
+
+  // const initialState = [];
+  // const todoState = useState(initialState)
+  // const [todos, setTodos] = useStateDevtools(todoState, initialState, 'TODOS');
 
   const handleChange = e =>{
     const {value, name} =  e.target;
 
-    setTask(value);  
+    setTask(value)
     console.log('value is ', value);
-    console.log('name is ', name);
 
   }
 
@@ -24,12 +30,11 @@ const App = () => {
     }
 
     console.log('newTask is ', newTask);  
-
-    setTodos([...todos, newTask]);
-    setTask('');
+    if(task) {
+      setTodos([...todos, newTask]);
+      setTask('');
+    }
     
-
-
   }  
 
   const clearTodos = () => {
@@ -42,36 +47,34 @@ const App = () => {
   }, [task, todos]); 
 
   return (
-    <div className="App">
+    <StateInspector>
+ 
+      <Button onClick = {clearTodos}> Clear All Todos</Button>
+        <Form onSubmit = {handleSubmit}>
+          <Label> New Todo Item</Label>
+          <Input
+            // label = 'new thing to do'
+            placeholder = 'the placeholer text'
+            value = {task}
+            onChange = {handleChange}
+          
+          />
+          <Button type = 'submit' > Add Todo </Button>
 
-    <div> something </div>  
-    <Button onClick = {clearTodos}> Clear All Todos</Button>
-      <Form onSubmit = {handleSubmit}>
-        <Label> New Todo Item</Label>
-        <Input
-          // label = 'new thing to do'
-          placeholder = 'the placeholer text'
-          // name = {task}
-          value = {task}
-          onChange = {handleChange}
-        
-        />
-        <Button type = 'submit' > Add Todo </Button>
+        </Form>
 
-      </Form>
+        <div>
+          {todos.map((item, index) => (
 
-      <div>
-        {todos.map((item, index) => (
+            <Card key = {index}>
+              <Label> {item.value} </Label>
+              <Label> {item.complete.toString()} </Label>
+            </Card>    
 
-          <Card key = {index}>
-            <Label> {item.value} </Label>
-            <Label> {item.complete} </Label>
-          </Card>    
-
-    ))}
-      </div>    
-
-    </div>
+      ))}
+        </div>    
+   
+    </StateInspector>
   );
 }
 
--- app/src/index.js
+++ app/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import {StateInspector} from 'reinspect';
 
-import addReactNDevTools from 'reactn-devtools';
-addReactNDevTools();
+// import addReactNDevTools from 'reactn-devtools';
+// addReactNDevTools();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+
+        <App />
+
+    
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
